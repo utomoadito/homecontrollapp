af8f6c2a2045caf18662f6609a81e6b7f60317b1
Teofilus Candra <teofiluschandra@gmail.com>
2019-10-26 13:13:21 +0800

    add feature remote control

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RemoteControl from './components/RemoteControl';
 
 function App() {
   return (
     <div className="App">
       <h2>Hello world</h2>
+      <RemoteControl />
     </div>
   );
 }
---
+++ src/components/RemoteControl.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RemoteControl = () => {
+    return (
+        <div className="RemoteControl">
+          <h2>Remote Control</h2>
+        </div>
+      );
+}
+
+export default RemoteControl
